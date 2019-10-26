e57f85d53a0b863f53fcb66bb3eae5fa0ef70231
Henry Ambicho T <yiperu@gmail.com>
2019-10-26 01:23:49 -0300

    In MonsterFragment impleent method onViewCreated where set setOnClickListener for feedMePizza buttom, where redirect navigation

--- app/src/main/java/com/raywenderlich/feedmenav/MonsterFragment.kt
+++ app/src/main/java/com/raywenderlich/feedmenav/MonsterFragment.kt
@@ -35,14 +35,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_monster.*
 
 class MonsterFragment : Fragment() {
 
-  override fun onCreateView(
-      inflater: LayoutInflater,
-      container: ViewGroup?,
-      savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_monster, container, false)
-  }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_monster, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        feedMePizza.setOnClickListener {
+            findNavController().navigate(R.id.navigation_pizza, null)
+        }
+    }
 }
