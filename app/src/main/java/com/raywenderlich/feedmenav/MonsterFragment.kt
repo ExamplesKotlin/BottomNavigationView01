48cbca51b4d224187406932a99432c010251a0b0
Henry Ambicho T <yiperu@gmail.com>
2019-10-26 01:30:55 -0300

    Add option for animation in MonsterFragment inside onViewCreated method

--- app/src/main/java/com/raywenderlich/feedmenav/MonsterFragment.kt
+++ app/src/main/java/com/raywenderlich/feedmenav/MonsterFragment.kt
@@ -36,6 +36,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import kotlinx.android.synthetic.main.fragment_monster.*
 
 class MonsterFragment : Fragment() {
@@ -50,6 +51,15 @@ class MonsterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
 
         feedMePizza.setOnClickListener {
             findNavController().navigate(R.id.navigation_pizza, null)
